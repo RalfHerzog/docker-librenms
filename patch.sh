a2c46f0241a77260881f2ad1656375d9a03ae98c
Ralf Herzog <ralf@rherzog.de>
2017-10-18 20:03:51 +0200

    Reset code on container start

--- patch.sh
+++ patch.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 cd /opt/librenms/
+
+git reset --hard HEAD
 for patch_file in /opt/patches/*.patch
 do
   echo "Apply patch: ${patch_file}"
   git apply --stat ${patch_file}
+  git apply --check ${patch_file}
   git apply ${patch_file}
 done
